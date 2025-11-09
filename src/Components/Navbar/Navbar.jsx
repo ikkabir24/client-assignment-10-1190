0bc03b51064648bb31508fa0d53284d1e9f64d78
imkabir54 <kkabir9900@gmail.com>
2025-11-09 09:07:44 +0600

    added dark mode

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { use } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import { Link, NavLink } from 'react-router';
 import userAvatar from '../../assets/user.png';
 import { AuthContext } from '../../provider/AuthProvider';
@@ -8,6 +8,14 @@ import { toast } from 'react-toastify';
 const Navbar = () => {
 
     const { user, logOut } = use(AuthContext);
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light')
+    
+    useEffect(()=>{
+        const html = document.querySelector('html');
+        html.setAttribute("data-theme", theme);
+        localStorage.setItem("theme", theme)
+    }, [theme])
+
     console.log(user)
 
     const links = <ul className='flex flex-col md:flex-row space-x-3'>
@@ -22,16 +30,21 @@ const Navbar = () => {
         <NavLink to={'/dashboard'}>Dashboard</NavLink>
     </ul>
 
-    const handleLogOut = () =>{
+    const handleLogOut = () => {
         logOut()
-        .then(result=>{
-            console.log(result);
-            toast('Logged out successfully..!')
-        })
-        .catch(error => {
-            toast(error.message);
-        })
+            .then(result => {
+                console.log(result);
+                toast('Logged out successfully..!')
+            })
+            .catch(error => {
+                toast(error.message);
+            })
     }
+
+    const handleTheme = (checked) => {
+        setTheme(checked ? 'dark' : 'light')
+    }
+
 
     return (
         <div className="navbar bg-base-100 shadow-sm">
@@ -60,8 +73,8 @@ const Navbar = () => {
                         <div className='p-1 rounded-[100%] bg-base-200'>
                             {
                                 user
-                                ? <img className='h-10 w-10 rounded-full' src={user.photoURL} />
-                                : <img className='h-10 w-10 rounded-full' src={userAvatar} />
+                                    ? <img className='h-10 w-10 rounded-full' src={user.photoURL} />
+                                    : <img className='h-10 w-10 rounded-full' src={userAvatar} />
                             }
                         </div>
                     </div>
@@ -70,11 +83,36 @@ const Navbar = () => {
                             ? <ul tabIndex="-1"
                                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow right-0 space-y-1">
                                 <li onClick={handleLogOut}><Link><LuLogOut /> Logout</Link></li>
+
+                                <li>
+                                    <div className='flex'>
+
+                                        <input
+                                            onChange={(e)=>handleTheme(e.target.checked)}
+                                            type="checkbox"
+                                            defaultChecked={localStorage.getItem('theme') === "dark"}
+                                            className="toggle" />
+                                        <p>Dark Mode</p>
+                                    </div>
+                                </li>
+
+
                             </ul>
                             : <ul tabIndex="-1"
                                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow right-0 space-y-1">
                                 <li><Link to={'/login'}><LuLogIn /> Login</Link></li>
                                 <li><Link to={'/register'}><LuUserRoundPlus /> Registration</Link></li>
+                                <li>
+                                    <div className='flex'>
+
+                                        <input
+                                            onChange={(e)=>handleTheme(e.target.checked)}
+                                            type="checkbox"
+                                            defaultChecked={localStorage.getItem('theme') === "dark"}
+                                            className="toggle" />
+                                        <p>Dark Mode</p>
+                                    </div>
+                                </li>
                             </ul>
 
                     }
